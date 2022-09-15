a77ff5e70cac8b7d118e63ae37d81b2d2b1b9888
Marty Nelson <marty@martypdx.com>
2022-09-15 11:36:41 -0700

    demo candies

---
+++ candies-data.js
@@ -0,0 +1,17 @@
+export const candies = [
+    {
+        name: 'Twix',
+        flavor: 'chocolate caramel',
+        type: 'candy bar',
+    },
+    {
+        name: 'Bubblicious',
+        flavor: 'strawberry watermelon',
+        type: 'gum',
+    },
+    {
+        name: 'Smarties',
+        flavor: 'tart fruity',
+        type: 'waffers',
+    },
+];
